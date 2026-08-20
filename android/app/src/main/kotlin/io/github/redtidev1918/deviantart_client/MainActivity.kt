a5ef878cff6834947d7c823d4ffcb2bd2c4b09d6
hezzn <hezzn@MacBook-Air.local>
2026-08-21 00:41:07 +0800

    feat: scaffold DeviantArt client UI with DAKit login and home

---
+++ android/app/src/main/kotlin/io/github/redtidev1918/deviantart_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.github.redtidev1918.deviantart_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test/widget_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:deviantart_client/main.dart';
+
+void main() {
+  testWidgets('shows client id hint when unconfigured', (WidgetTester tester) async {
+    await tester.pumpWidget(const DeviantArtClientApp());
+    await tester.pump();
+    expect(find.text('Pass DAKIT_CLIENT_ID at build time.'), findsOneWidget);
+  });
+}
